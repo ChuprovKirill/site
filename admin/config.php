25addf9f560f6d73288fc8b5ce00f0f9887c0d44
Chuprov Kirill <Chuprov Kirill>
2017-02-09 21:59:07 +0300

    Initial commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.sok-store.ru/admin/');
+define('HTTP_CATALOG', 'http://www.sok-store.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.sok-store.ru/admin/');
+define('HTTPS_CATALOG', 'http://www.sok-store.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/home/httpd/vhosts/sok-store.ru/httpdocs/admin/');
+define('DIR_SYSTEM', '/home/httpd/vhosts/sok-store.ru/httpdocs/system/');
+define('DIR_IMAGE', '/home/httpd/vhosts/sok-store.ru/httpdocs/image/');
+define('DIR_LANGUAGE', '/home/httpd/vhosts/sok-store.ru/httpdocs/admin/language/');
+define('DIR_TEMPLATE', '/home/httpd/vhosts/sok-store.ru/httpdocs/admin/view/template/');
+define('DIR_CONFIG', '/home/httpd/vhosts/sok-store.ru/httpdocs/system/config/');
+define('DIR_CACHE', '/home/httpd/vhosts/sok-store.ru/httpdocs/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/httpd/vhosts/sok-store.ru/httpdocs/system/storage/download/');
+define('DIR_LOGS', '/home/httpd/vhosts/sok-store.ru/httpdocs/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/httpd/vhosts/sok-store.ru/httpdocs/system/storage/modification/');
+define('DIR_UPLOAD', '/home/httpd/vhosts/sok-store.ru/httpdocs/system/storage/upload/');
+define('DIR_CATALOG', '/home/httpd/vhosts/sok-store.ru/httpdocs/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'a180321.mysql.mchost.ru');
+define('DB_USERNAME', 'a180321_base');
+define('DB_PASSWORD', 'Ljrnjhdfybx92');
+define('DB_DATABASE', 'a180321_base');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://www.sok-store.ru/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://www.sok-store.ru/');
+
+// DIR
+define('DIR_APPLICATION', '/home/httpd/vhosts/sok-store.ru/httpdocs/catalog/');
+define('DIR_SYSTEM', '/home/httpd/vhosts/sok-store.ru/httpdocs/system/');
+define('DIR_IMAGE', '/home/httpd/vhosts/sok-store.ru/httpdocs/image/');
+define('DIR_LANGUAGE', '/home/httpd/vhosts/sok-store.ru/httpdocs/catalog/language/');
+define('DIR_TEMPLATE', '/home/httpd/vhosts/sok-store.ru/httpdocs/catalog/view/theme/');
+define('DIR_CONFIG', '/home/httpd/vhosts/sok-store.ru/httpdocs/system/config/');
+define('DIR_CACHE', '/home/httpd/vhosts/sok-store.ru/httpdocs/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/httpd/vhosts/sok-store.ru/httpdocs/system/storage/download/');
+define('DIR_LOGS', '/home/httpd/vhosts/sok-store.ru/httpdocs/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/httpd/vhosts/sok-store.ru/httpdocs/system/storage/modification/');
+define('DIR_UPLOAD', '/home/httpd/vhosts/sok-store.ru/httpdocs/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'a180321.mysql.mchost.ru');
+define('DB_USERNAME', 'a180321_base');
+define('DB_PASSWORD', 'Ljrnjhdfybx92');
+define('DB_DATABASE', 'a180321_base');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
